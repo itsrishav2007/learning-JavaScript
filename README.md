98f3cf3d28189b6250eedcd1d9012f95a37a899c
Rishav majhi006 <rishavmajhi@rishav-ka-MacBook-Air.local>
2025-09-16 17:23:04 +0545

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # learning-JavaScript
+# learning-JavaScript
--- script.js
+++ script.js
@@ -1,66 +1,14 @@
-console.log(" Hello JS");
- x = null;
- y = undefined;
- console.log(x);
- console.log(y);
-
-//we are learning about the data types
-
-//NUMBER TYPES
- age =20;
- price = 100.10;
-
-//on a compiler
-age 
-'20'
-typeof age 
-'number'
-
-
-//STRING TYPES
-fullName = "rishav";
-
-//on a compiler
-fullName
-'rishav'
-typeof fullName
-'string'
-
-//BOOLEAN TYPES
-isFollow = true;
-
-//on a compiler
-isFollow
-true
-typeof isFollow
-'boolean'
-
-//UNDEFINED TYPES
-let x; 
-
-//on a compiler
-x
-undefined
-
-// NULL TYPES
-let x = null;
-
-//on a compiler
-x
-null
-typeof x
-'object'
-
-//bigInt and Symbol
-let x = BigInt("123");
-let y =Symbol("hello!");
-
-//on a compiler
-x
-123n           //in the last there always come n.
-typeof x
-'bigint'
-y
-Symbol(hello!)
-typeof y
-'symbol'
+//OBJECT
+const student = {
+  fullName: "rishav",
+  age:20,
+  cgpa:9.1,
+  isPass:true
+  //key:value pairs
+};
+console.log(student);       //printing object
+console.log(student.fullName);
+student["age"] = student["age"] + 1;   //updating age
+console.log(student.age);
+student["fullName"] = "ram";     //updating name
+console.log(student.fullName);
