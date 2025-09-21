f87a9d1d34a226a6598901d90f6bfe418ef5014d
Rishav majhi006 <rishavmajhi@rishav-ka-MacBook-Air.local>
2025-09-21 13:15:06 +0545

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # learning-JavaScript
 # learning-JavaScript
 # learning-JavaScript
+# learning-JavaScript
---
+++ conditionalSt.js
@@ -0,0 +1,102 @@
+// //LOGICAL OPERATORS
+
+// let a = 10;
+// let b = 5;
+
+// let cond1 = a > b ;
+// let cond2 = a!=b ;
+
+// console.log("cond1 && cond2 = " , cond1 && cond2);
+// console.log("cond1 || cond2 = " , cond1 || cond2);
+
+//CONDITIONAL STATEMENTS
+
+//IF STATEMENT
+
+//let age = 19;
+// if(age < 18){
+//   console.log("you are minor");
+// }
+// if(age >= 18 ){
+//   console.log("you are adult");
+// }
+
+
+
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark"){
+//   color = "black";
+// }
+// if( mode === "light"){
+// color = "white";
+// }
+// console.log(color);
+
+//IF-ELSE STATEMENT
+
+// if(mode === "dark"){
+//   color = "black";
+// }
+// else{
+//   color = "white";
+// }
+// console.log(color);
+
+
+//CHECK EVEN OR ODD NUMBER
+
+// let a = 4;
+// let number;
+// if(a % 2 === 0){
+//   number = "even";
+// }
+// else{
+//   number = "odd";
+// }
+// console.log(number);
+
+// //ELSE-IF STATEMENT
+
+// let age = 25;
+// let category;
+
+// if(age <18){
+//   category ="minor";
+// }
+// else if(age >=18 && age < 60){
+//   category = " adult";  
+// }
+// else{
+//   category = "senior citizen";
+// }
+// console.log(category);
+
+// let mode = "dark";
+// let result;
+
+// if (mode === "dark" ){
+//   result = "black"  ;
+// }
+// else if(mode === "light"){
+//   result = "white";
+// }
+// else if(mode === "blue"){
+//   result = "blue";
+// }
+// else{
+//   result = "unknown";
+// }
+// console.log(result);
+
+
+
+//TERNARY OPERATOR
+
+let age = 25;
+
+ let result=age >=18 ? "adult" : "not adult";
+  console.log(result);
+  
+
